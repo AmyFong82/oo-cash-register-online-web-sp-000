006712d4c368ee416a9f75920951f26d017fe5ca
Amy Fong <amyhfong@gmail.com>
2020-01-30 14:54:07 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,32 @@
+class CashRegister
+  attr_accessor :total, :discount, :quantity, :items
 
+  @@items =[]
+
+
+  def initialize(discount = 0)
+    @total = 0
+    self.discount = 20
+  end
+
+  def add_item(title, price, quantity = 1)
+      @total += price * quantity
+      @@items.fill(title, @@items.size, quantity)
+  end
+
+  def apply_discount
+    if @total == 0
+      return "There is no discount to apply."
+    else
+      @total *= 0.8
+      return "After the discount, the total comes to $#{@total.to_i}."
+    end
+  end
+
+  def items
+    @@items
+  end
+
+
+
+end
